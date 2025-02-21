1888aa2606e3063401c55db703f14be666f688aa
lassombra <briatt22@gmail.com>
2025-02-20 19:21:59 -0600

    Fix wrong operator

--- Orbitersdk/samples/ProjectApollo/src_csm/saturn.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/saturn.cpp
@@ -5104,7 +5104,7 @@ void Saturn::UpdateMassAndCoG()
 		//lights
 		SpotLight->UpdatePosition(CoGShift);
 		RndzLight->UpdatePosition(CoGShift);
-		optics->UpdatePosition(CoGShift);
+		optics.UpdatePosition(CoGShift);
 
 		// All done!
 		LastFuelWeight = CurrentFuelWeight;
